1003ff8743d1721669308d242ff06830bbae288a
parkinyeop <parkinyeop@naver.com>
2022-10-14 12:55:20 +0900

    Update RandomIdleSelector.cs

--- Assets/Scripts/Character/RandomIdleSelector.cs
+++ Assets/Scripts/Character/RandomIdleSelector.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class RandomIdleSelector : StateMachineBehaviour
 {
-    
+
 
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(!animator.IsInTransition(0))
-        animator.SetInteger("IdleSelect", RandomeSelect());
+        if (!animator.IsInTransition(0))
+            animator.SetInteger("IdleSelect", RandomeSelect());
     }
 
     int RandomeSelect()
     {
-        int selelct = Random.Range(0, 5);
+        int randomIdle;
+        int select;
+        randomIdle = Random.Range(0, 101);
+        if (randomIdle <= 3)
+        {
+            select = 5;
+        }
+        else if (randomIdle <= 6)
+        {
+            select = 4;
+        }
+        else if (randomIdle <= 10)
+        {
+            select = 3;
+        }
+        else if (randomIdle <= 30)
+        {
+            select = 2;
+        }
+        else
+        {
+            select = 1;
+        }
 
-        return selelct;
+        return select;
     }
 
 
