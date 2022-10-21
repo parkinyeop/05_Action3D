fb4715f8a0672dc1bb720a768b2296fac67f6341
parkinyeop <parkinyeop@naver.com>
2022-10-21 14:02:46 +0900

    HP 문자 정상 출력

--- Assets/Scripts/Test/Test_Battle.cs
+++ Assets/Scripts/Test/Test_Battle.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class Test_Battle :  Test_Base
 {
     Player player;
     private void Start()
     {
         player = GameManager.Inst.Player;
+        
     }
     protected override void Test1(InputAction.CallbackContext _)
     {
--- Assets/Scripts/UI/PlayerHP.cs
+++ Assets/Scripts/UI/PlayerHP.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHP : MonoBehaviour
 {
+    public TMP_Text hpText;
     Slider slider;
+    Player player;
 
     private void Awake()
     {
@@ -14,12 +17,14 @@ public class PlayerHP : MonoBehaviour
     }
     private void Start()
     {
-        Player player = GameManager.Inst.Player;
+        player = GameManager.Inst.Player;
         player.onHealthChange += OnHealthChange;
+        //Debug.Log(player.hp);
     }
 
     private void OnHealthChange(float ratio)
     {
+        hpText.text = $"{player.hp}/{player.maxHp}";
         ratio = Mathf.Clamp(ratio,0,1);
         slider.value = ratio;
     }
