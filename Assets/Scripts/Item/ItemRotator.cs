594f531800757c1302943ec924f34544f875c56f
parkinyeop <parkinyeop@naver.com>
2022-10-27 13:14:30 +0900

    itemRotator 만들기

---
+++ Assets/Scripts/Item/ItemRotator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRotator : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float minHeight;
+    public float maxHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
