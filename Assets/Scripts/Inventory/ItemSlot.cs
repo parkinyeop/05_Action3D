45c224ab438aa93e693cc0a55e1979d81e1e60c5
parkinyeop <parkinyeop@naver.com>
2022-11-03 09:23:08 +0900

    Update ItemSlot.cs
    property 생성

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,39 @@ public class ItemSlot
     ItemData slotsItemData = null;
     uint itemCount = 0;
 
+    /// <summary>
+    /// 슬롯에 들어있는 아이템 데이터 프로퍼티
+    /// </summary>
+    public ItemData ItemData
+    {
+        get => slotsItemData;
+        private set
+        {
+            if (slotsItemData != value) //슬롯 아이템이 변경 있을때
+            {
+                slotsItemData = value;
+                onSlotItemChange?.Invoke(); // 델리게이트에서 연결된 함수들 실행(UI갱신용)
+            }
+        }
+    }
+
+    public uint ItemCount
+    {
+        get => itemCount;
+        private set
+        {
+            if(itemCount != value)  // 아이템 갯수가 변경 있을때
+            {
+                itemCount = value;
+                onSlotItemChange.Invoke();
+            }
+        }
+    }
+
+    public Action onSlotItemChange;
+
     public bool IsEmpty => slotsItemData == null;
     public uint Index => slotIndex;
-
-    public ItemData ItemData => slotsItemData;
-
-    public uint ItemCount => itemCount;
-
     public ItemSlot(uint index)
     {
         slotIndex = index;
@@ -23,36 +50,48 @@ public class ItemSlot
 
     public void AssignSlotItem(ItemData data, uint count = 1)
     {
-        itemCount = count;
-        slotsItemData = data;
+        ItemCount = count;
+        ItemData = data;
         Debug.Log($"인벤토리 {Index}번 슬롯에 {data.itemName} 아이템 추가");
     }
 
     public void ClearSlotItem()
     {
-        slotsItemData = null;
-        itemCount = 0;
+        ItemData = null;
+        ItemCount = 0;
         Debug.Log($"인벤토리 {Index}번 슬롯 아이템 삭제");
     }
-
+    /// <summary>
+    /// 슬롯의 아이템 증가
+    /// </summary>
+    /// <param name="count"> 갯수 </param>
     public void IncreaseSlotItem(uint count = 1)
     {
-        itemCount += count;
-        Debug.Log($"인벤토리 {Index}번 슬롯에 {slotsItemData.itemName} 아이템 {count}개 증가");
+        if (!IsEmpty)
+        {
+            ItemCount += count;
+            Debug.Log($"인벤토리 {Index}번 슬롯에 {ItemData.itemName} 아이템 {count}개 증가");
+        }
     }
-
+    /// <summary>
+    /// 슬롯의 아이템 감소
+    /// </summary>
+    /// <param name="count"> 갯수 </param>
     public void DecreaseSlotItem(uint count = 1)
     {
-        int newCount = (int)ItemCount - (int)count;
+        if (!IsEmpty)
+        {
+            int newCount = (int)ItemCount - (int)count;
 
-        if(newCount < 0)
-        {
-            ClearSlotItem();
-        }
-        else
-        {
-            itemCount -= (uint)newCount;
-            Debug.Log($"인벤토리 {Index}번 슬롯에 {slotsItemData.itemName} 아이템 {count}개 감소");
+            if (newCount < 0)
+            {
+                ClearSlotItem();
+            }
+            else
+            {
+                ItemCount -= (uint)newCount;
+                Debug.Log($"인벤토리 {Index}번 슬롯에 {ItemData.itemName} 아이템 {count}개 감소");
+            }
         }
     }
 }
