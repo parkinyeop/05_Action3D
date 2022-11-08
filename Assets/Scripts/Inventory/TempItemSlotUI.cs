72a88f1cac061018b789e865b7bd4592356241ad
parkinyeop <parkinyeop@naver.com>
2022-11-08 12:52:19 +0900

    드래그이동

--- Assets/Scripts/Inventory/TempItemSlotUI.cs
+++ Assets/Scripts/Inventory/TempItemSlotUI.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TempItemSlotUI : ItemSlotUI
 {
+    private void Start()
+    {
+        Vector2 screen = Mouse.current.position.ReadValue();
+    }
+
+
     public void Open()
     {
         if(!ItemSlot.IsEmpty)
