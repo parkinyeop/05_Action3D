16e0b782c62439a413f9fd22ab35df54805c6fb0
parkinyeop <parkinyeop@naver.com>
2022-11-08 11:27:46 +0900

    아이템 슬롯 클릭

--- Assets/Scripts/Inventory/ItemSlotUI.cs
+++ Assets/Scripts/Inventory/ItemSlotUI.cs
@@ -4,19 +4,29 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
+using System;
 
-public class ItemSlotUI : MonoBehaviour
+public class ItemSlotUI : MonoBehaviour, IDragHandler,IBeginDragHandler,IPointerUpHandler,IPointerClickHandler
 {
     uint id;        //몇번째 슬롯인가
     protected ItemSlot itemSlot;    // UI와 연결된 아이템 슬롯
+
+    TextMeshProUGUI itemCountText;
     Image itemImage;
+
     public uint ID => id;
     public ItemSlot ItemSlot => itemSlot;
-    public TextMeshProUGUI itemCountText;
+
+
+    public Action<uint> onDragStart;
+    public Action<uint> onMouseUp;
+
 
     private void Awake()
     {
         itemImage = transform.GetChild(0).GetComponent<Image>();
+        itemCountText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
     }
     /// <summary>
     /// 슬롯 초기화 함수
@@ -38,11 +48,13 @@ public class ItemSlotUI : MonoBehaviour
         {
             itemImage.sprite = null;
             itemImage.color = Color.clear;
+            itemCountText.text = null;
         }
         else
         {
             itemImage.sprite = ItemSlot.ItemData.itemIcon;
             itemImage.color = Color.white;
+            itemCountText.text = ItemSlot.ItemCount.ToString();
         }
     }
 
@@ -50,5 +62,36 @@ public class ItemSlotUI : MonoBehaviour
     {
         RectTransform rectTransform = (RectTransform)itemImage.gameObject.transform;
         rectTransform.sizeDelta = new Vector2(iconSize, iconSize);
+    }
+    /// <summary>
+    /// 다른 마우스 이벤트를 사용하기 위해 OnDrag는 반드시 있어야 한다
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log($"OnDrag : {eventData.delta}");
+
+        // eventData.position : 마우스 포인터의 스크린좌표값
+        // eventData.delta : 마우스 포인터의 위치 변화량
+        // eventData.button == PointerEventData.InputButton.Left : 마우스 버튼 왼쪽이 눌려있다
+        // eventData.button == PointerEventData.InputButton.Right : 마우스 버튼 오른쪽이 눌려있다
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+        Debug.Log($"OnDragStart : {ID}");
+        onDragStart?.Invoke(ID);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log($"OnPointUp : {ID}");
+        onMouseUp?.Invoke(ID);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
     }
 }
