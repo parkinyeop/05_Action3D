72e6894d5531469b9369af83b2ccd486c9984401
parkinyeop <parkinyeop@naver.com>
2022-11-17 12:31:56 +0900

    아이템 미니맵 표시

--- Assets/Scripts/Test/Test_Battle.cs
+++ Assets/Scripts/Test/Test_Battle.cs
@@ -29,15 +29,15 @@ public class Test_Battle :  Test_Base
     }
     protected override void Test4(InputAction.CallbackContext _)
     {
-        GameObject[] objs = ItemFactory.MakeItem(ItemIdCode.Coin_Bronze, 2);
-        GameObject[] objs2 = ItemFactory.MakeItem(ItemIdCode.Coin_Silver, 3, new Vector3(0, 0, 2f));
-        GameObject[] objs3 = ItemFactory.MakeItem(ItemIdCode.Coin_Gold,5, new Vector3(1, 0, 2f), true);
-        GameObject[] objs4 = ItemFactory.MakeItem(ItemIdCode.HealingPotion,5, new Vector3(1, 0, 2f), true);
-        GameObject[] objs5 = ItemFactory.MakeItem(ItemIdCode.ManaPotion,5, new Vector3(1, 0, 2f), true);
-        GameObject[] objs6 = ItemFactory.MakeItem(ItemIdCode.IronSword,1, new Vector3(1, 0, 2f), true);
-        GameObject[] objs7 = ItemFactory.MakeItem(ItemIdCode.SilverSword,1, new Vector3(1, 0, 2f), true);
-        GameObject[] objs8 = ItemFactory.MakeItem(ItemIdCode.RoundSheild,1, new Vector3(1, 0, 2f), true);
-        GameObject[] objs9 = ItemFactory.MakeItem(ItemIdCode.IronSheild,1, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs = ItemFactory.MakeItem(ItemIdCode.Coin_Bronze, 2);
+        //GameObject[] objs2 = ItemFactory.MakeItem(ItemIdCode.Coin_Silver, 3, new Vector3(0, 0, 2f));
+        //GameObject[] objs3 = ItemFactory.MakeItem(ItemIdCode.Coin_Gold, 5, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs4 = ItemFactory.MakeItem(ItemIdCode.HealingPotion, 5, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs5 = ItemFactory.MakeItem(ItemIdCode.ManaPotion, 5, new Vector3(1, 0, 2f), true);
+        GameObject[] objs6 = ItemFactory.MakeItem(ItemIdCode.IronSword, 1, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs7 = ItemFactory.MakeItem(ItemIdCode.SilverSword, 1, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs8 = ItemFactory.MakeItem(ItemIdCode.RoundSheild, 1, new Vector3(1, 0, 2f), true);
+        //GameObject[] objs9 = ItemFactory.MakeItem(ItemIdCode.IronSheild, 1, new Vector3(1, 0, 2f), true);
 
     }
 
